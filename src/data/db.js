38b9a671039a4160616f812eb975602c88c2ee11
arbridev <arebrif@gmail.com>
2020-05-28 09:12:53 -0400

    Setup for production Fix error: self signed certificate

--- src/data/db.js
+++ src/data/db.js
@@ -1,4 +1,4 @@
-// process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 const pg = require('pg');
 pg.defaults.ssl = process.env.DATABASE_URL ? true : false;
